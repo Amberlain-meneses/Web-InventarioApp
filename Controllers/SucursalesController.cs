2197fb322e412ab0a03fecbf77ae92cea8801ab4
Amberlain Meneses <amberlain98@gmail.com>
2020-10-29 21:52:17 -0600

    version 1

---
+++ Controllers/SucursalesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InventarioApp.Models;
+
+namespace InventarioApp.Controllers
+{
+    public class SucursalesController : Controller
+    {
+        private InventarioDBEntities db = new InventarioDBEntities();
+
+        // GET: Sucursales
+        public ActionResult Index()
+        {
+            return View(db.Sucursales.ToList());
+        }
+
+        // GET: Sucursales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursales sucursales = db.Sucursales.Find(id);
+            if (sucursales == null)
+            {
+                return HttpNotFound();
+            }
+            //Ver nuestros productos disponibles
+
+            ViewBag.mproduct = db.SP_viewProducts(id).ToList();
+            return View(sucursales);
+        }
+
+        // GET: Sucursales/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sucursales/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_sucursal,nombre")] Sucursales sucursales)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sucursales.Add(sucursales);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sucursales);
+        }
+
+        // GET: Sucursales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursales sucursales = db.Sucursales.Find(id);
+            if (sucursales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursales);
+        }
+
+        // POST: Sucursales/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_sucursal,nombre")] Sucursales sucursales)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sucursales).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sucursales);
+        }
+
+        // GET: Sucursales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursales sucursales = db.Sucursales.Find(id);
+            if (sucursales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursales);
+        }
+
+        // POST: Sucursales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sucursales sucursales = db.Sucursales.Find(id);
+            db.Sucursales.Remove(sucursales);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AgregarStock(int id_producto, int id_sucursal, int cantidad)
+        {
+            //Actuliza nuestro stock cuando se agrega nuevos registros
+            db.SP_agregarStock(id_producto, id_sucursal, cantidad);
+            return RedirectToAction("Index");
+        }
+
+            //Actualiza nuestro stock cuando se hace una venta
+        public ActionResult DesckStock(int id_producto, int id_sucursal, int cantidad)
+        {
+            db.SP_descStock(id_producto, id_sucursal, cantidad);
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
